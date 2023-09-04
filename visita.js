9699d9f19c2d13d87a48a9631500c46091cf0f88
JuliaDouraddo <141073505+JuliaDouraddo@users.noreply.github.com>
2023-09-04 16:34:23 -0400

    Update visita.js

--- visita.js
+++ visita.js
@@ -1,1 +1,22 @@
 
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.img');
+
+function changeImage(direction) {
+  currentImageIndex += direction;
+  if (currentImageIndex < 0) {
+    currentImageIndex = images.length - 1;
+  } else if (currentImageIndex >= images.length) {
+    currentImageIndex = 0;
+  }
+  showImage(currentImageIndex);
+}
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    image.style.display = i === index ? 'block' : 'none';
+  });
+}
+
+showImage(currentImageIndex);
+
