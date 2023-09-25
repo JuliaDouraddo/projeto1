2590dd891581583445fd26062842e95228cc5bd2
JuliaDouraddo <141073505+JuliaDouraddo@users.noreply.github.com>
2023-09-25 13:49:02 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,1 +1,51 @@
+const audio = document.getElementById('audio');
+const playPauseButton = document.getElementById('play-pause-button');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
 
+const songs = [
+    'radiomp3/musica1.mp3',
+    'radiomp3/muscia2.mp3',
+    'radiomp3/musica3.mp3',
+    'radiomp3/musica4.mp3',
+    'radiomp3/musica5.mp3',
+    'radiomp3/musica6.mp3',
+    'radiomp3/musica7.mp3',
+    'radiomp3/musica8.mp3',
+    'radiomp3/musica9.mp3',
+    'radiomp3/musica10.mp3',
+];
+
+let currentSongIndex = 0;
+
+function loadSong() {
+    audio.src = songs[currentSongIndex];
+}
+
+function playPause() {
+    if (audio.paused) {
+        audio.play();
+        playPauseButton.textContent = 'Pause';
+    } else {
+        audio.pause();
+        playPauseButton.textContent = 'Play';
+    }
+}
+
+function playNext() {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    loadSong();
+    audio.play();
+}
+
+function playPrev() {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    loadSong();
+    audio.play();
+}
+
+loadSong();
+
+playPauseButton.addEventListener('click', playPause);
+nextButton.addEventListener('click', playNext);
+prevButton.addEventListener('click', playPrev);
